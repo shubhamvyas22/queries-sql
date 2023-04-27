aa172768b7373b9011ce9fb875e28ea15ff1ab1d
Shubham Vyas <shubhamvyas@Shubhams-MacBook-Pro.local>
2023-04-27 14:25:35 +0530

    added a couple of columns

--- Misc/bharati-query-april-2023.sql
+++ Misc/bharati-query-april-2023.sql
@@ -1,8 +1,10 @@
 SELECT
+  t1._merchant_key,
   t2.commercial_subregion_code,
   t1.latest_annualized_gmv_band, 
   t1.country_name,
-  sum(t3.annual_gmv) as region_gmv,
+  DATE_DIFF(CURRENT_DATE(),t1.first_contract_start_date,month) AS age_in_months,
+  sum(t3.annual_gmv) as shopify_gmv,
   count(t1._merchant_key) as merchant_count,
   count(t1.current_shop_count) as shop_count
 FROM
@@ -27,4 +29,4 @@ WHERE
 t1.geographic_region_code = "APAC"
 and t1.is_included_in_historical_reporting = True
 and t1.is_active = True
-group by 1,2,3 
+group by 1,2,3,4,5
